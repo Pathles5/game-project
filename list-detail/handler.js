2fce94a9ffac7f678a29041f8a8858b370e6fb80
aslopez <alopez@soaint.com>
2022-03-31 00:19:23 +0200

    Pasos 1, 2 y 3 de Tour of Heroes

--- list-detail/handler.js
+++ list-detail/handler.js
@@ -1,23 +1,30 @@
 "use strict";
+const axios = require('axios');
 
 module.exports.hello = async (event) => {
   let inputs = {
-      fields : ["name","id","platforms"],
-      where : {"platforms":["42"]},
-      exclude : ["name"],
-      limit: "2",
-      sort: ["id"],
-      desc: true,
-      search: "Kratos" 
-    };
-    formQueryToIGDB(inputs);
+    fields: ["name", "id", "platforms"],
+    where: { "platforms": ["42"] },
+    exclude: ["name"],
+    limit: "2",
+    sort: ["id"],
+    desc: true,
+    search: "Kratos"
+  };
+  formQueryToIGDB(inputs);
 
+  let inputReal = "fields age_ratings,category,keywords,name,parent_game,platforms,similar_games,slug,status,summary,tags,url,version_parent,version_title,videos,websites;"
+
+  let listGames = await listPagination(4, 5, inputReal);
+  
+  let result = await findListGameById(["10"], 10)
+  console.log(result)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        input: result,
       },
       null,
       2
@@ -44,17 +51,65 @@ module.exports.hello = async (event) => {
  */
 function formQueryToIGDB(inputs) {
   let fields = (inputs.fields) ? "fields " + inputs.fields.join(",") + "; " : null;
-  let where=null;
-  if (inputs.where){
+  let where = null;
+  if (inputs.where) {
     const keys = Object.keys(inputs.where);
-    where = keys.map(key => key + "= (" + inputs.where[key].join(",")+")")
-    where = where.join(",") ;
+    where = keys.map(key => key + "= (" + inputs.where[key].join(",") + ")")
+    where = where.join(",");
     where = "where " + where + "; ";
   }
+
+  let exclude = (inputs.exclude) ? "exclude " + inputs.exclude.join(",") + "; " : "";
+  let limit = "limit " + ((inputs.limit) ? inputs.limit : "500") + "; ";
+  let sort = (inputs.sort) ? ("sort " + inputs.sort.join(",") + ((inputs.desc) ? " desc" : " asc") + ";") : "";
+  let search = (inputs.search) ? "search \"" + inputs.search + "\";" : "";
+  //console.log(fields + where + exclude + limit + sort + search);
+  let result = fields + where + exclude + limit + sort + search;
+  return result;
+}
+
+async function listPagination(startItem, amountPag, query) {
+  query = (startItem > 0) ? `${query} offset ${startItem};` : query;
+  query = (amountPag) ? `${query} limit ${amountPag};` : query;
+  return post(query);
+}
+
+/**
+ * 
+ * @param {string[]} id 
+ */
+async function findListGameById(id, amountPag) {
+  let query = {
+    fields: ["id", "name", "age_ratings"],
+    where: { "id": id },
+    limit: amountPag ?? 500
+  };
+  query = formQueryToIGDB(query);
+  console.log("Query")
+  console.log(query)
+  const result = await post(query);
   
-  let exclude = (inputs.exclude) ? "exclude " + inputs.exclude.join(",") + "; " : null;
-  let limit = "limit "+((inputs.limit) ? inputs.limit : "500")+"; ";
-  let sort = (inputs.sort) ? ("sort " + inputs.sort.join(",") + ((inputs.desc) ? " desc" : " asc") + ";") : null;
-  let search = (inputs.search) ? "search \"" + inputs.search + "\";" :  null;
-  console.log(fields+where+exclude+limit+sort+search);
+  console.log("Result")
+  console.log(result)
+  return result;
+}
+
+async function post(query) {
+  return await axios({
+    url: "https://api.igdb.com/v4/games",
+    method: 'POST',
+    headers: {
+      //'Accept': 'application/json',
+      'Client-ID': 't3xi1wknh11kke1zb5wcsmat5a2jdh',
+      'Authorization': 'Bearer kj9eodl42oqhhjcjxmjdt9ibpbunp8',
+    },
+    data: query
+  })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.error(err);
+      return err;
+    });
 }
